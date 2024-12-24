a82e95815c4701457200f63ee0489a586608eaf7
nahiankhan7 <thenewkhan7@gmail.com>
2024-12-24 12:57:56 +0600

    fix disconnect problem

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
       <h1>VNC Client ReactJS</h1>
       <VncViewer
         url="ws://vnc-5900.steamer.qaclan.com"
-        viewOnly={false}
+        viewOnly={true}
         width="100%"
         height="100%"
         onError={(error) => console.error("Error connecting to VNC", error)}
